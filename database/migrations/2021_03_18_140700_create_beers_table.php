0b16661e6bfcadf9e248f150dd8d8da3401b1a52
Francesco <delta873@gmail.com>
2021-03-18 15:29:26 +0100

    Creata tabella beers tramite migration

---
+++ database/migrations/2021_03_18_140700_create_beers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beers', function (Blueprint $table) {
+            $table->id();
+            $table->string('beer_typology',100);
+            $table->float('price', 6,2);
+            $table->string('brand',200);
+            $table->string('nationality',100);
+            $table->float('liters', 2,1);
+            $table->string('image',2000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beers');
+    }
+}
