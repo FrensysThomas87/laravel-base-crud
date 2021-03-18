7b783142d299cadb7ca2a6c2be10c09c36e61c0c
Francesco <delta873@gmail.com>
2021-03-18 18:10:13 +0100

    Recuperato id su show

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,5 @@ use Illuminate\Support\Facades\Route;
 
 */
 
-Route::get('/', 'BeerController@index');
-Route::get('/beer', 'BeerController@show');
+//Route::get('/', 'BeerController@index');
+Route::resource('/beers', 'BeerController');
