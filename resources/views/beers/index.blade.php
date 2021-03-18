42f7f0ebbabad484f1937e4275f8d472deaa7571
Francesco <delta873@gmail.com>
2021-03-18 22:18:26 +0100

    Stilizzata pagina del singolo prodotto

--- resources/views/beers/index.blade.php
+++ resources/views/beers/index.blade.php
@@ -6,36 +6,40 @@
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
         </head>
         <body>
-        <table class="table">
-            <thead class="thead-dark">
-                <tr>
-                <th scope="col">#</th>
-                <th scope="col">Beer Typology</th>
-                <th scope="col">Price</th>
-                <th scope="col">Brand</th>
-                <th scope="col">Nationality</th>
-                <th scope="col">Liters</th>
-                <th scope="col">Image</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($beers as $beer)
-                <tr>
-                    <th scope="row">{{$beer->id}}</th>
-                    <td>{{$beer->beer_typology}}</td>
-                    <td>{{$beer->price}}</td>
-                    <td>{{$beer->brand}}</td>
-                    <td>{{$beer->nationality}}</td>
-                    <td>{{$beer->liters}}</td>
-                    <div class="img-container">
-                        <td><img src="{{$beer->image}}"/></td>
-                    </div>
-                </tr>
-            </tbody>
-                @endforeach
-        </table>
-    </body>
-</html>
+            <div class="container">
+                <div class="row">
+                    <table class="table">
+                        <thead class="thead-dark">
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Beer Typology</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Brand</th>
+                            <th scope="col">Nationality</th>
+                            <th scope="col">Liters</th>
+                            <th scope="col">Image</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($beers as $beer)
+                            <tr>
+                                <th scope="row">{{$beer->id}}</th>
+                                <td>{{$beer->beer_typology}}</td>
+                                <td>{{$beer->price}}</td>
+                                <td>{{$beer->brand}}</td>
+                                <td>{{$beer->nationality}}</td>
+                                <td>{{$beer->liters}}</td>
+                                <div class="img-container">
+                                    <td><img src="{{$beer->image}}"/></td>
+                                </div>
+                            </tr>
+                        </tbody>
+                            @endforeach
+                    </table>
+                </div>
+            </div>      
+        </body>
+    </html>
 
 
 
--- resources/views/beers/show.blade.php
+++ resources/views/beers/show.blade.php
@@ -1,1 +1,44 @@
-{{$beer}};
+    <!DOCTYPE html>
+        <html lang="en" dir="ltr">
+        <head>
+        <meta charset="utf-8">
+        <title>Laravel Base Crud</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        </head>
+        <body>
+            <div class="container">
+                <div class="row">
+                    <table class="table">
+                        <thead class="thead-dark">
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Beer Typology</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Brand</th>
+                            <th scope="col">Nationality</th>
+                            <th scope="col">Liters</th>
+                            <th scope="col">Image</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        
+                            <tr>
+                                <th scope="row">{{$beer->id}}</th>
+                                <td>{{$beer->beer_typology}}</td>
+                                <td>{{$beer->price}}</td>
+                                <td>{{$beer->brand}}</td>
+                                <td>{{$beer->nationality}}</td>
+                                <td>{{$beer->liters}}</td>
+                                <div class="img-container">
+                                    <td><img src="{{$beer->image}}"/></td>
+                                </div>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </body>
+    </html>
+
+
+
